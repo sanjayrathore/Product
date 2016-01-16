2a1ad2244052da55edf06e6e7fe6c5f7a5fd6c46
Sanjay Rathore <sanjay.r@cisinlabs.com>
2016-01-16 20:02:35 +0530

    config commit

--- application/config/config.php
+++ application/config/config.php
@@ -16,7 +16,7 @@
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/learn/codeIgniter/';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
